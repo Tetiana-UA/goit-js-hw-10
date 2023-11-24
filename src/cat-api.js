ae4643a7c171b5afd7321dbeada00cb97571220f
Tetiana-UA <izidaxm85@gmail.com>
2023-11-24 22:50:41 +0100

    4

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,4 @@
-
-
+//Напиши функцію fetchBreeds(), яка виконує HTTP-запит і повертає проміс із масивом порід - результатом запиту. Винеси її у файл cat-api.js та зроби іменований експорт.
 export function fetchBreeds() {
     const BASE_URL  = "https://api.thecatapi.com/v1";
     const ENDPOINT = "breeds"
@@ -9,22 +8,33 @@ export function fetchBreeds() {
         api_key:APY_KEY,
     })
 
-return fetch(`${BASE_URL}/${ENDPOINT}?${params}`)
+return fetch(`${BASE_URL}/${ENDPOINT}?${params}`).then((response)=>{
+    if (!response.ok) {
+        throw new Errow("404 not found!");
+    } 
+    return response.json(); 
+    })
 
 }
 
 
-
-export function fetchCatByBreed(breed) {
+//Напиши функцію fetchCatByBreed(breedId), яка очікує ідентифікатор породи, робить HTTP-запит і повертає проміс із даними про кота - результатом запиту. Винеси її у файл cat-api.js і зроби іменований експорт.
+export function fetchCatByBreed(id) {
     const BASE_URL  = "https://api.thecatapi.com/v1";
     const ENDPOINT = "images/search"
     const APY_KEY ="live_KhQV1oKIS4BCvrcPDeozPUb3E9SMkjS0ykizMr5iBCwxr7OS8E0iDtuzHjzD1OGJ"
 
     const params=new URLSearchParams({
         api_key:APY_KEY,
-        breed_ids:breed,
+        breed_ids:id,
     })
 
-return fetch(`${BASE_URL}/${ENDPOINT}?${params}`)
-
+return fetch(`${BASE_URL}/${ENDPOINT}?${params}`).then((response)=>{
+    if (!response.ok) {
+    throw new Errow("404 not found!");
+    } 
+    return response.json(); 
+})
 }
+
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //HTTP-запити
-
 //Використовуй публічний The Cat API. 
 
 
@@ -11,29 +10,15 @@
 //Додай мінімальне оформлення елементів інтерфейсу.
 
 
-// В И К О Н А Н Н Я
+//                           В И К О Н А Н Н Я
+
 //Для використання ключа необхідно використовувати HTTP-заголовок x-api-key. Рекомендується використовувати axios та додати заголовок до всіх запитів.
 import axios from "axios";
-
 axios.defaults.headers.common["x-api-key"] = "live_KhQV1oKIS4BCvrcPDeozPUb3E9SMkjS0ykizMr5iBCwxr7OS8E0iDtuzHjzD1OGJ";
 
-
-
-//https://api.thecatapi.com/v1/images/search?breed_ids={breed.id}
-
-
-
-
-
-//import { fetchBreeds, fetchCatByBreed } from "./src/cat-api.js";
-console.log(fetchBreeds());
-console.log(fetchCatByBreed());
-
-
-
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
 
 const refs = {
-    form: document.querySelector(".search-form"),
     breedSelect: document.querySelector(".breed-select"),
     loader: document.querySelector(".loader"),
     error: document.querySelector(".error"),
@@ -41,82 +26,45 @@ const refs = {
 }
 
 //Під час завантаження сторінки має виконуватися HTTP-запит за колекцією порід. Для цього необхідно виконати GET-запит на ресурс https://api.thecatapi.com/v1/breeds, що повертає масив об'єктів. У разі успішного запиту, необхідно наповнити select.breed-select опціями так, щоб value опції містило id породи, а в інтерфейсі користувачеві відображалася назва породи.
-function onLoad () {
+
+
+function onLoad() {
     fetchBreeds()
     .then(({breeds})=>{
     refs.breedSelect.innerHTML = createMarkupForSelect(breeds);
     })
     .catch((err)=>{
-        console.log(err);
+    console.log(err);
     })
-    .finally(()=>form.reset());
-    }
-
+    .finally(()=>refs.breedSelect.reset());
+}
+    console.log(onLoad());
        //breed.value=id!!!!!!!!
 
 
 refs.breedSelect.addEventListener("submit", handleSubmit);
 
 //Коли користувач обирає якусь опцію в селекті, необхідно виконувати запит за повною інформацією про кота на ресурс https://api.thecatapi.com/v1/images/search. Не забудь вказати в цьому запиті параметр рядка запиту breed_ids з ідентифікатором породи.
-//Якщо запит був успішний, під селектом у блоці div.cat-info з'являється зображення і розгорнута інформація про кота: назва породи, опис і темперамент.
 function handleSubmit(event) {
     event.preventDefault();
-    console.log(event.currentTarget.elements);
     const form = event.currentTarget;
     const { breed } = form.elements;
 
 
     fetchCatByBreed(breed.value)
+
     .then(({breeds})=>{
     refs.catInfo.innerHTML = createMarkup(breeds);
     })
     .catch((err)=>{
-        console.log(err);
+    console.log(err);
     })
     .finally(()=>form.reset());
     }
 
 
 
-//Напиши функцію fetchBreeds(), яка виконує HTTP-запит і повертає проміс із масивом порід - результатом запиту. Винеси її у файл cat-api.js та зроби іменований експорт.
- function fetchBreeds() {
-    const BASE_URL  = "https://api.thecatapi.com/v1";
-    const ENDPOINT = "breeds"
-    const APY_KEY ="live_KhQV1oKIS4BCvrcPDeozPUb3E9SMkjS0ykizMr5iBCwxr7OS8E0iDtuzHjzD1OGJ"
 
-    const params=new URLSearchParams({
-        api_key:APY_KEY,
-    })
-
-return fetch(`${BASE_URL}/${ENDPOINT}?${params}`).then((response)=>{
-    if (!response.ok) {
-        throw new Errow("404 not found!");
-    } 
-    return response.json(); 
-    })
-
-}
-
-
-//Напиши функцію fetchCatByBreed(breedId), яка очікує ідентифікатор породи, робить HTTP-запит і повертає проміс із даними про кота - результатом запиту. Винеси її у файл cat-api.js і зроби іменований експорт.
- function fetchCatByBreed(breed) {
-    const BASE_URL  = "https://api.thecatapi.com/v1";
-    const ENDPOINT = "images/search"
-    const APY_KEY ="live_KhQV1oKIS4BCvrcPDeozPUb3E9SMkjS0ykizMr5iBCwxr7OS8E0iDtuzHjzD1OGJ"
-
-    const params=new URLSearchParams({
-        api_key:APY_KEY,
-        breed_ids:breed,
-    })
-
-return fetch(`${BASE_URL}/${ENDPOINT}?${params}`).then((response)=>{
-    if (!response.ok) {
-    throw new Errow("404 not found!");
-    } 
-    return response.json(); 
-})
-
-}
 
 function createMarkup(arr) {
     return arr
@@ -135,9 +83,14 @@ function createMarkup(arr) {
 function createMarkupForSelect(arr) {
     return arr
     .map(({
-        id
+        id,name
     })=>` 
-    <option value="id">${id}</option>` 
+    <option value="${id}">${name}</option>` 
     )
     .join("");
 }
+
+
+
+
+
