0325eae2aa31d7fef6c6d552f416645d6e1cdc3b
Tetiana-UA <izidaxm85@gmail.com>
2023-11-29 17:46:42 +0100

    9

--- src/index.js
+++ src/index.js
@@ -77,12 +77,12 @@ function createMarkup(data) {
         const{name, description, temperament}=data[0].breeds[0]; //деструктуризуємо властивості обєкта в масиві breeds, який у свою чергу знаходиться в масиві data. Обидва масиви містять лише по одному обєкту, тому до них звертаємося під індексом [0].
 
     return `<div class="cat-card">
-    <img src="${url}" alt="${name} width="550" height="350>
+    <img src="${url}" alt="${name} width="600" height="400>
     <div class="cat-card-info">
         <h1>${name}</h1>
         <p>${description}</p>
         <p><span class="temperament">Temperament: </span> ${temperament}</p>
-        </div>
+    </div>
     </div>` 
 }       
 
